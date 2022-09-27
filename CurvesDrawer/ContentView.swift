3cb2f4f91477581c283c96b1cd7cb9176372adf4
serhii.lomov <serhii.lomov@itomy.ch>
2022-09-27 03:14:12 +0300

    Builder decomposition issue. Unbuildeable.

--- CurvesDrawer/ContentView.swift
+++ CurvesDrawer/ContentView.swift
@@ -59,56 +59,62 @@ struct ContentView: View {
     @DrawgramBuilder var curves: [DrawableCurve] {
         switch symbol {
         case .flower:
-            DB.AddThread {
-                let leafLeft = BezierCurve(x0: 0, y0: 0, x1: -0.15, y1: -0.1, x2: -0.25, y2: -0.3, x3: 0, y3: -0.5)
-                
-                TB.Thread.Start(curve: leafLeft)
-                TB.Repeat.SelfMirrored(reversion: true)
-                for _ in 1..<leafsCount {
-                    TB.Repeat.Rotated(last: 2, angle: -.dpi / CGFloat(leafsCount))
-                }
-            }
+            flower()
         case .celtic:
-            DB.AddThread {
-                let topRightCorner = BezierCurve
-                    .arc(from: .pi * 0.75, to: .pi * -0.25, radius: 0.5)
-                    .scaled(x: cornerRadius, y: cornerRadius)
-                    .translated(x: cornerOffset, y: -1 * cornerOffset)
-                    .smoothed(mult1: 1.5, mult2: 1.5)
+            celtic()
+        }
+    }
 
-                TB.Thread.Name("Diagonal1")
-    //            topRightCorner
-    //            BezierCurve.line(from: topRightCorner.p3, to: topRightCorner.p0.mirrored())
-                TB.Thread.Start(curve: topRightCorner)
-                TB.Continue.Line(to: topRightCorner.p0.mirrored())
-                TB.Crossing(.bottom, .top, .bottom, .top)
-                TB.Repeat.Mirrored()
-                if stepByStep { TB.Timestamp(0.5) }
+    @DrawgramBuilder func flower() -> [DrawableCurve] {
+        DB.AddThread {
+            let leafLeft = BezierCurve(x0: 0, y0: 0, x1: -0.15, y1: -0.1, x2: -0.25, y2: -0.3, x3: 0, y3: -0.5)
+
+            TB.Thread.Start(curve: leafLeft)
+            TB.Repeat.SelfMirrored(reversion: true)
+            for _ in 1..<leafsCount {
+                TB.Repeat.Rotated(last: 2, angle: -.dpi / CGFloat(leafsCount))
+            }
+        }
+    }
+
+    @DrawgramBuilder func celtic() -> [DrawableCurve] {
+        DB.AddThread {
+            let topRightCorner = BezierCurve
+                .arc(from: .pi * 0.75, to: .pi * -0.25, radius: 0.5)
+                .scaled(x: cornerRadius, y: cornerRadius)
+                .translated(x: cornerOffset, y: -1 * cornerOffset)
+                .smoothed(mult1: 1.5, mult2: 1.5)
+
+            TB.Thread.Name("Diagonal1")
+            TB.Thread.Start(curve: topRightCorner)
+            TB.Continue.Line(to: topRightCorner.p0.mirrored())
+            TB.Crossing(.bottom, .top, .bottom, .top)
+            TB.Repeat.Mirrored()
+            if stepByStep { TB.Timestamp(0.5) }
+        }
+
+        DB.RotatedThread(source: "Diagonal1", name: "Diagonal2", angle: .hpi)
+        DB.InverseThreadCrossing(name: "Diagonal2")
+
+        DB.AddThread {
+            let topCorner = BezierCurve
+                .arc(from: .pi * 0.75, to: .pi * 0.25, radius: 0.5)
+                .scaled(x: centerRadius, y: centerRadius)
+                .translated(x: 0, y: -1 * (0.5 - centerRadius / 2))
+
+            TB.Thread.Name("OutLoop")
+            if stepByStep { TB.Timestamp(0.5) }
+            TB.Thread.Start(curve: topCorner)
+
+            if sharps {
+                TB.Continue.Line(to: topCorner.p0.rotated(angle: .hpi))
+                TB.Repeat.Rotated(angle: .hpi)
+            } else {
+                TB.Continue.Line(to: topCorner.p0.rotated(angle: -.hpi))
+                TB.Repeat.Rotated(angle: -.hpi)
             }
 
-            DB.RotatedThread(source: "Diagonal1", name: "Diagonal2", angle: .hpi)
-            DB.InverseThreadCrossing(name: "Diagonal2")
-
-            DB.AddThread {
-                let topCorner = BezierCurve
-                    .arc(from: .pi * 0.75, to: .pi * 0.25, radius: 0.5)
-                    .scaled(x: centerRadius, y: centerRadius)
-                    .translated(x: 0, y: -1 * (0.5 - centerRadius / 2))
-
-                TB.Thread.Name("OutLoop")
-                if stepByStep { TB.Timestamp(0.5) }
-                TB.Thread.Start(curve: topCorner)
-
-                if sharps {
-                    TB.Continue.Line(to: topCorner.p0.rotated(angle: .hpi))
-                    TB.Repeat.Rotated(angle: .hpi)
-                } else {
-                    TB.Continue.Line(to: topCorner.p0.rotated(angle: -.hpi))
-                    TB.Repeat.Rotated(angle: -.hpi)
-                }
-
-                TB.Repeat.Mirrored()
-            }
+            TB.Repeat.Mirrored()
         }
     }
 }
